6e9a45f8c8a9f9d2f6bb0f6ff41f6b02cb1f3bc4
Daniel Kunicki <dankunicki@gmail.com>
2017-10-22 22:35:53 +0200

    Upload current version

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    public bool spawnTrigger = false;
+
+    List<GameObject> spawnPlaces = new List<GameObject>();
+
+	// Use this for initialization
+	void Start () {
+        GameObject spawnPlacesParent = GameObject.Find("PlayerSpawnPoints");
+        foreach (Transform child in spawnPlacesParent.transform)
+        {
+            spawnPlaces.Add(child.gameObject);
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(spawnTrigger)
+        {
+            Spawn();
+        }
+	}
+
+    private void Spawn () {
+        GameObject spawnPlace = spawnPlaces[Random.Range(0, spawnPlaces.Count)];
+        transform.position = spawnPlace.transform.position;
+        spawnTrigger = false;
+    }
+}
