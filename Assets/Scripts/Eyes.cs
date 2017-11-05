8b1fc1e8e87c71cff363e3fbd7a0b18cb1447b43
Daniel Kunicki <dankunicki@gmail.com>
2017-11-05 22:03:34 +0100

    Add Zoom and Calling helicopter sound

---
+++ Assets/Scripts/Eyes.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Eyes : MonoBehaviour {
+
+    private Camera eyes;
+    private float defaultFOV;
+    public float zoomSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+        eyes = GetComponent<Camera>();
+        defaultFOV = eyes.fieldOfView;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetButton("Zoom"))
+        {
+            if (eyes.fieldOfView > defaultFOV / 1.5f)
+            {
+                eyes.fieldOfView -= Time.deltaTime * zoomSpeed;
+            }
+                      
+        } else
+        {
+            if (eyes.fieldOfView < defaultFOV)
+            {
+                eyes.fieldOfView += Time.deltaTime * 2 * zoomSpeed;
+            }
+        }
+
+     
+    }
+}
---
+++ Assets/Scripts/Radio.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Radio : MonoBehaviour {
+
+    private AudioSource audioSource;
+
+	// Use this for initialization
+	void Start () {
+        audioSource = GetComponent<AudioSource>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("CallHeli") && !audioSource.isPlaying)
+        {
+            audioSource.Play();
+        }
+	}
+}
